dd11f4a5c358d0553a9f8527d25cbb307041a48f
Dinar2202 <dinar2202@gmail.com>
2025-11-17 15:11:23 +0300

    feat: table migration tariffs

--- src/postgres/migrations/20251117_create_tariffs_table.ts
+++ src/postgres/migrations/20251117_create_tariffs_table.ts
@@ -1,8 +1,24 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('tariffs', (column) => {
-    column.increments('id').primary();
-    column.
-  })
+    await knex.schema.createTable("tariffs", (table) => {
+        table.increments("id").primary();
+        table.date("date").notNullable();
+        table.string("warehouse_name").notNullable();
+        table.string("geo_name").nullable();
+        table.decimal("box_storage_liter").nullable();
+        table.decimal("box_storage_base").nullable();
+        table.decimal("box_storage_coef_expr").nullable();
+        table.decimal("box_delivery_base").nullable();
+        table.decimal("box_delivery_liter").nullable();
+        table.decimal("box_delivery_coef_expr").nullable();
+        table.decimal("box_delivery_marketplace_liter").nullable();
+        table.decimal("box_delivery_marketplace_coef_expr").nullable();
+        table.decimal("box_delivery_marketplace_base").nullable();
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("tariffs");
 }
