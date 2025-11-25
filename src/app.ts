362fc0dec57aed81c0422dea7734c7c3fe32f541
Dinar2202 <dinar2202@gmail.com>
2025-11-25 21:47:49 +0300

    feat: a well-developed function with node-cron

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,31 @@
-import knex, { migrate, seed } from "#postgres/knex.js";
-import { WBApiService } from "./services/wb_api.service.js";
-import { saveTariffs } from "./postgres/knex.js";
+import { migrate, seed } from "#postgres/knex.js";
+import { fetchTariffsJob } from "./jobs/fetchTariffs.job.js";
+import { updateGoogleSheetsJob } from "./jobs/updateGoogleSheets.job.js";
+import cron from "node-cron";
+import env from "#config/env/env.js";
 
 async function main() {
+    console.log("Приложение запускается...");
+
+    console.log("Выполняется первичная миграция таблицы в базу данных...");
     await migrate.latest();
     await seed.run();
-    console.log("All migrations and seeds have been run");
+    console.log("Все миграции и сиды были успешно выполнены ");
 
-    const wbService = new WBApiService();
-    const result = await wbService.getWBTariffs();
+    await fetchTariffsJob();
+    await updateGoogleSheetsJob();
 
-    await saveTariffs(result);
+    cron.schedule(env.UPDATE_CRON, async () => {
+        await fetchTariffsJob();
+        await updateGoogleSheetsJob();
+    });
+
+    console.log(`
+    Планировщик запущен по расписанию "${env.UPDATE_CRON}"
+    `);
 }
 
-main();
+main().catch((err) => {
+    console.error("Ошибка при запуске приложения:", err);
+    process.exit(1);
+});
